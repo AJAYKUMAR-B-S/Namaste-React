0f9d7d2ed0799c39e3b43b393b0c302274fad7fc
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-07-29 21:23:39 +0530

    Episode-02 : Clone the Episode-01 index.html, style.css, and script.js files to the Episode-02 Coding folder for further coding.

---
+++ Episode-02--Igniting-our-APP/Coding/script.js
@@ -0,0 +1,40 @@
+{
+  /* <div class="parentDiv">
+  <div class="childDiv1">
+    <h1 class="heading1">I am h1</h1>
+    <h2 class="heading2">I am h2</h2>
+  </div>
+  <div class="childDiv1">
+    <h1 class="heading1">I am h1</h1>
+    <h2 class="heading2">I am h2</h2>
+  </div>
+  </div> 
+  */
+}
+
+const parentDiv = React.createElement(
+  "div",
+  { id: "parentDiv" },
+  React.createElement("div", { id: "childDiv1" }, [
+    React.createElement("h1", { id: "heading1" }, "I am h1"),
+    React.createElement("h2", { id: "heading2" }, "I am h2"),
+  ]),
+  React.createElement("div", { id: "childDiv2" }, [
+    React.createElement("h3", { id: "heading3" }, "I am h3"),
+    React.createElement("h4", { id: "heading4" }, "I am h4"),
+  ])
+);
+
+console.log(parentDiv);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React"
+);
+
+console.log(heading); //Object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
