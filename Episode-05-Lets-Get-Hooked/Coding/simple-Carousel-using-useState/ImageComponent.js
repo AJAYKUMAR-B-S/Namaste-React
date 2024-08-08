cba9a5b03b7e7fa3c26a8c27914aa9ee3df866f0
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-08 23:03:05 +0530

    Episode-05 : Create Image Component and Img URL variable

---
+++ Episode-05-Lets-Get-Hooked/Coding/simple-Carousel-using-useState/ImageComponent.js
@@ -0,0 +1,10 @@
+export const URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const Image = (props) => {
+  return (
+    <>
+      <img src={props.addres} alt="gallery_Img"></img>
+    </>
+  );
+};
