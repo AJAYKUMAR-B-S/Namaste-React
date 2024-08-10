a7df84185e1f120972b7cd64aac3ecb2cb04df0a
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-10 14:23:55 +0530

    Episode-06 : Create Shimmer Component and Export it

---
+++ Episode-06-Exploring-the-World/Coding/src/Components/Shimmer.js
@@ -0,0 +1,17 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer_Container">
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+      <div className="shimmer_animation_Container"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
