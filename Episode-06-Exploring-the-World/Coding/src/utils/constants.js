07aca8798b6752721406038c71e83ac69777e998
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-10 23:45:23 +0530

    Episode-06 : Remove Old Logo URL Variable from constants.js

--- Episode-06-Exploring-the-World/Coding/src/utils/constants.js
+++ Episode-06-Exploring-the-World/Coding/src/utils/constants.js
@@ -1,5 +1,2 @@
-export const APP_LOGO =
-  "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717113600&semt=ais_user";
-
 export const RESTAURANT_CARD_IMG_CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
