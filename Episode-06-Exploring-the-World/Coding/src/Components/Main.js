f3d70061d7447348469402660b681728f42d4198
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-10 23:55:28 +0530

    Episode-06 : Import the Search Icon, Add Search UI Feature , and Style It in CSS File

--- Episode-06-Exploring-the-World/Coding/src/Components/Main.js
+++ Episode-06-Exploring-the-World/Coding/src/Components/Main.js
@@ -1,6 +1,7 @@
 import Restaurantcard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer.js";
+import { FaSearch } from "react-icons/fa";
 
 const Main = () => {
   const [apiData, setapiData] = useState([]);
@@ -21,39 +22,50 @@ const Main = () => {
 
   return (
     <main className="main">
-      <div className="filter_Container">
-        <span>Filter : </span>
-        <button
-          className="filter_button"
-          onClick={() => {
-            const filterApiData = apiData.filter(
-              (data) => data.info.avgRating > 4.3
-            );
-            setapiData(filterApiData);
-          }}
-        >
-          Top rated Restaurant
-        </button>
-        <button
-          className="filter_button"
-          onClick={() => {
-            fetchData();
-          }}
-        >
-          All Restaurant
-        </button>
+      <div className="searching_Container">
+        <div className="search_Box_Div">
+          <input
+            type="text"
+            className="search_Input"
+            placeholder="search for restaurant and food"
+          ></input>
+          <FaSearch className="search_Icon" />
+        </div>
       </div>
-      <div className="restaurants_Card_Container">
-        {/* //Conditional Rendering */}
-        {apiData.length === 0 ? (
-          <Shimmer />
-        ) : (
-          apiData.map((data) => {
-            return (
-              <Restaurantcard key={data.info.id} restaurantData={data.info} />
-            );
-          })
-        )}
+      <div className="restaurants_Card_Filter_Container">
+        <div className="filter_Container">
+          <button
+            className="filter_button"
+            onClick={() => {
+              const filterApiData = apiData.filter(
+                (data) => data.info.avgRating > 4.3
+              );
+              setapiData(filterApiData);
+            }}
+          >
+            Top rated
+          </button>
+          <button
+            className="filter_button"
+            onClick={() => {
+              fetchData();
+            }}
+          >
+            All
+          </button>
+        </div>
+        <div className="restaurants_Card_Container">
+          {/* //Conditional Rendering */}
+          {apiData.length === 0 ? (
+            <Shimmer />
+          ) : (
+            apiData.map((data) => {
+              return (
+                <Restaurantcard key={data.info.id} restaurantData={data.info} />
+              );
+            })
+          )}
+        </div>
       </div>
     </main>
   );
