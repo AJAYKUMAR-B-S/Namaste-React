814290ba296a996a34ccf8db33d368932fa72d95
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-21 15:19:34 +0530

    Episode-07 : Import About Component and createBrowserRouter from react

--- Episode-07-Finding-the-Path/Coding/src/script.js
+++ Episode-07-Finding-the-Path/Coding/src/script.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import About from "./Components/About";
+import { createBrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
