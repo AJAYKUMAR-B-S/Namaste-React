a66ce3b4bf90e6b11dbd3621e5e6a237e1a4ec64
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-24 20:00:56 +0530

    Episode-07 :  Create RestaurantMenu Component and Tried of useParams 🪝hook which import from react-route-dom

---
+++ Episode-07-Finding-the-Path/Coding/src/Components/RestaurantMenu.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const RestaurantMenu = () => {
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  const fetchingData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=426730&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const toJson = await data.json();
+  };
+  return (
+    <div className="restaurantMenu_Container">
+      <h1>This is restaurantMenu Page</h1>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
