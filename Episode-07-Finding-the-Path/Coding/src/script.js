82ec791c2792751af7b2418805800d4c59ee2695
Ajaykumar b s <ajaykumarbirursathish@gmail.com>
2024-08-24 20:03:05 +0530

    Episode-07 : Import RestaurantMenu Component and  create path inside the createBrowserRouter and add resMenu Component to the element attribute

--- Episode-07-Finding-the-Path/Coding/src/script.js
+++ Episode-07-Finding-the-Path/Coding/src/script.js
@@ -5,6 +5,7 @@ import Main from "./Components/Main";
 import About from "./Components/About";
 import ErrorComponent from "./Components/Error";
 import Contact from "./Components/Contact";
+import RestaurantMenu from "./Components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -27,6 +28,7 @@ const appRouter = createBrowserRouter([
       { path: "/", element: <Main /> },
       { path: "/about", element: <About /> },
       { path: "/contact", element: <Contact /> },
+      { path: "/restaurant/:Id", element: <RestaurantMenu /> },
     ],
     errorElement: <ErrorComponent />,
   },
