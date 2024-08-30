78e72ad8f3e27a9342acde3d0103eb851c41d9e0
AjayKumar B S <ajaykumarbirursathish@gmail.com>
2024-08-30 10:29:25 +0530

    Update Error.js

--- Episode-07-Finding-the-Path/Coding/src/Components/Error.js
+++ Episode-07-Finding-the-Path/Coding/src/Components/Error.js
@@ -2,8 +2,6 @@ import { useRouteError } from "react-router-dom";
 
 const ErrorComponent = () => {
   const error = useRouteError();
-
-  console.log(error);
   return (
     <div className="error_Container">
       <h1>Page Not found</h1>
